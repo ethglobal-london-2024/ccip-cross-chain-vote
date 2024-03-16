8952a5e5d8990c19c03a89a2e4e0543bb281b498
Stan Trenev <stan@0xfusion.com>
2024-03-16 09:38:00 +0000

    feat: add DestinationVoter contract

--- contracts/DestinationVoter.sol
+++ contracts/DestinationVoter.sol
@@ -1,4 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-contract DestinationVoter {}
+import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {IGovernor} from "@openzeppelin/contracts/governance/IGovernor.sol";
+
+contract DestinationVoter is CCIPReceiver {
+    IGovernor governor;
+
+    event VoteSuccessful();
+
+    constructor(address router, address governorAddress) CCIPReceiver(router) {
+        governor = IGovernor(governorAddress);
+    }
+
+    function _ccipReceive(Client.Any2EVMMessage memory message) internal override {
+        (bool success,) = address(governor).call(message.data);
+        require(success);
+        emit VoteSuccessful();
+    }
+}
